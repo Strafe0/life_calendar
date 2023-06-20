b701add8f46a0f55749b5495058d8c017f3cfd60
ValeriyGolyshev <valeriy1golyshev@gmail.com>
2023-06-20 14:01:14 +0700

    Added dialog when back button pressed in calendar screen

--- lib/views/calendar/calendar_screen.dart
+++ lib/views/calendar/calendar_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:life_calendar/views/calendar/calendar_widget.dart';
 import 'package:life_calendar/controllers/calendar_controller.dart';
 import 'package:life_calendar/setup.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({Key? key}) : super(key: key);
@@ -20,29 +22,58 @@ class _CalendarScreenState extends State<CalendarScreen> with SingleTickerProvid
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('Календарь жизни в неделях'),
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(onPressed: () => Navigator.pushNamed(context, '/thanks'), icon: Icon(Icons.handshake_outlined, color: Theme.of(context).iconTheme.color)),
-        ],
-      ),
-      body: InteractiveViewer(
-        maxScale: 5,
-        child: SafeArea(
-          child: CalendarWidget(),
+    return WillPopScope(
+      onWillPop: () async {
+        return await showDialog<bool>(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Выход'),
+            content: const Text('Выйти из приложения?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  if (Platform.isAndroid) {
+                    SystemNavigator.pop();
+                  } else if (Platform.isIOS) {
+                    exit(0);
+                  }
+                },
+                child: const Text('Да'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+                child: const Text('Нет'),
+              ),
+            ],
+          ),
+        ) ?? false;
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Календарь жизни в неделях'),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(onPressed: () => Navigator.pushNamed(context, '/thanks'), icon: Icon(Icons.handshake_outlined, color: Theme.of(context).iconTheme.color)),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Перейти к текущей неделе',
-        child: const Icon(Icons.location_searching),
-        onPressed: () {
-          var week = controller.currentWeek;
-          controller.selectWeek(week.id, week.yearId);
-          Navigator.pushNamed(context, '/weekInfo');
-        },
+        body: InteractiveViewer(
+          maxScale: 5,
+          child: SafeArea(
+            child: CalendarWidget(),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Перейти к текущей неделе',
+          child: const Icon(Icons.location_searching),
+          onPressed: () {
+            var week = controller.currentWeek;
+            controller.selectWeek(week.id, week.yearId);
+            Navigator.pushNamed(context, '/weekInfo');
+          },
+        ),
       ),
     );
   }
